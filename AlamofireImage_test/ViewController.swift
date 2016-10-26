21ec73135061e589b4bae1ae9fac84a4dde7ca45
Jaime Andrade <jaimeandrade454@gmail.com>
2016-10-26 17:27:00 -0500

    init

--- AlamofireImage_test/ViewController.swift
+++ AlamofireImage_test/ViewController.swift
@@ -7,13 +7,30 @@
 //
 
 import UIKit
+import AlamofireImage
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request("https://httpbin.org/image/png").responseImage { response in
+            debugPrint(response)
+            print(response.request)
+            print(response.response)
+            debugPrint(response.result)
+            
+            if let image = response.result.value {
+                print("image downloaded: \(image)")
+            }
+        }
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+           }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
